03713857cab7018a513de017c9c225c9ec8f074a
KJTx1 <jasonyanzhizhen@gmail.com>
2020-06-20 16:43:27 -0700

    Upload working webblocker with 3 blocked example webs

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.webRequest.onBeforeRequest.addListener(
+  function(info) {
+    console.log("Intercepted");
+    return {redirectUrl: "https://leetcode.com"};
+  },
+  // filters
+  {urls: url},
+  // extraInfoSpec
+  ["blocking"]);
