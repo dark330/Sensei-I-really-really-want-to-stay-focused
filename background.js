9e1f8a40ac639fb9e172026045a11e3b8b76baf0
yx99 <yx99@uw.edu>
2020-06-21 16:00:26 -0700

    fixed merge conflict

--- background.js
+++ background.js
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
-  });
-});
-=======
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {
     console.log("Intercepted");
@@ -20,4 +9,4 @@ chrome.webRequest.onBeforeRequest.addListener(
   {urls: url},
   // extraInfoSpec
   ["blocking"]);
->>>>>>> 03713857cab7018a513de017c9c225c9ec8f074a
+
