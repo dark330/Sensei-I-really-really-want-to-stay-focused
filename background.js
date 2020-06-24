a4c1ff0a819540a45e71a1b7e887ac080781211d
KJTx1 <jasonyanzhizhen@gmail.com>
2020-06-23 22:34:03 -0700

    Update working global flag for on/off extension

--- background.js
+++ background.js
@@ -1,14 +1,37 @@
 'use strict';
 
-let STUDY_MODE = false;
+var STUDY_MODE = false;
+
+// var popup = chrome.extension.getViews({
+//   type: "popup"
+// });
+
+// // for (var i = 0; i < popup.length; i++) {
+// var onOff = popup[0].document.getElementById("switch");
+// // }
+
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {
-    console.log("Intercepted");
-    console.log(info);
-    return {redirectUrl: "https://leetcode.com"};
+    if (STUDY_MODE) {
+      console.log("Intercepted");
+      console.log(info);
+      return {redirectUrl: "https://leetcode.com"};
+    }
   },
   // filters
   {urls: url},
   // extraInfoSpec
   ["blocking"]);
+
+function backgroundFunction() {
+  if (STUDY_MODE) {
+    STUDY_MODE = false;
+  } else {
+    STUDY_MODE = true;
+  }
+}
+
+function getState() {
+  return STUDY_MODE;
+}
--- popup.js
+++ popup.js
@@ -1,1 +1,22 @@
 "use strict"
+
+var onOff = document.getElementById("switch");
+var bg = chrome.extension.getBackgroundPage()
+
+if (bg.getState()) {
+    onOff.textContent = "Stop";
+} else {
+    onOff.textContent = "Start";
+}
+
+onOff.addEventListener("click", (e) => {
+    
+    if (bg.getState()) {
+        bg.backgroundFunction();
+        onOff.textContent = "Start";
+    } else {
+        bg.backgroundFunction();
+        onOff.textContent = "Stop";
+    }
+
+});
